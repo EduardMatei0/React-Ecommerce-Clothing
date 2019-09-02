6ce1acb407320585cefec151117fc8c85eb3a130
matei.eduard <eduardmatei0@yahoo.com>
2019-09-02 18:36:58 +0300

    add saga

---
+++ src/pages/collection/collection.container.jsx
@@ -0,0 +1,17 @@
+import { connect } from "react-redux";
+import { compose } from "redux";
+import { createStructuredSelector } from "reselect";
+import { selectisCollectionsLoaded } from "../../redux/shop/shop.selectors";
+import WithSpinner from "../../components/with-spinner/with-spinner.component";
+import CollectionPage from "./collection.component";
+
+const mapStateToProps = createStructuredSelector({
+  isLoading: state => !selectisCollectionsLoaded(state)
+});
+
+const CollectionPageContainer = compose(
+  connect(mapStateToProps),
+  WithSpinner
+)(CollectionPage);
+
+export default CollectionPageContainer;
