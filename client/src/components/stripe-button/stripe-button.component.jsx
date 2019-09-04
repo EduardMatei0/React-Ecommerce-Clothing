208cf2e0a203caf73ea2e1839dc86a7f4cc6de73
matei.eduard <eduardmatei0@yahoo.com>
2019-09-04 16:00:09 +0300

    add back end

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = "pk_test_1fBrMRpImNzsaTqMdfVcJ8bb00YPipJTGo";
 
   const onToken = token => {
-    console.log(token);
-    alert("Payment Succesful!");
+    axios({
+      url: "payment",
+      method: "POST",
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then(response => alert("Payment successfull"))
+      .catch(error => {
+        alert(
+          "There was an issue with your payment. Please make sure you use a valid credit card!"
+        );
+        console.log("Payment error: ", JSON.parse(error));
+      });
   };
 
   return (
